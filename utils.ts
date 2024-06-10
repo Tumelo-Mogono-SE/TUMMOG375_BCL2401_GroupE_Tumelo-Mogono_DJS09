8c69d17ba4b2aac13e2a8cb046ad15c8a3226c5d
Tumelo Mogono <tumelomogono@gmail.com>
2024-06-10 23:44:42 +0200

    Added a function to my utils file which takes is a parameter which uses the types from the interface which is of type Review which I imported from my interfaces file.

--- utils.ts
+++ utils.ts
@@ -2,6 +2,7 @@ const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
 const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
 const userNameDisplay = document.querySelector('#user') as HTMLElement
 import {LoyaltyUser, Permissions} from './enums'
+import { Review } from './interfaces'
 
 export function showReviewTotal (value: number, reviewer: string, isLoyalty: LoyaltyUser) {
     const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
@@ -27,5 +28,10 @@ export function makeMultiple(value: number) : string {
     if (value > 1 || value == 0 ) {
         return 's'
     } else return ''
+}
+
+export function getTopTwoReviews(reviews : Review[]) : Review[]  {
+ const sortedReviews = reviews.sort((a, b) => b.stars - a.stars)
+ return sortedReviews.slice(0,2)
 }
 
