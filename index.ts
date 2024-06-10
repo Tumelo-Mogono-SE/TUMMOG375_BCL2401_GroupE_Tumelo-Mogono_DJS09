927c5b4e5f44e0dd7cc5b48fbe09f61fcb58b979
Tumelo Mogono <tumelomogono@gmail.com>
2024-06-10 13:44:19 +0200

    Added type to my fetched HTML element and then added a function which takes in two parameters one being a number type and a string which inside it I assign to the innerHTML of my fetched html element the sentance to add strings with value which we convert to string and another sentence with another string we get from our argument string. I then parse the arguments using my reviews array.

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 // Write a function that will only accept numbers and attend to 
 // all TypeScript weakness flags.
 // : number
-const reviewTotalDisplay = document.querySelector('#reviews')
+const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement 
 
 const reviews = [
     {
@@ -24,3 +24,9 @@ const reviews = [
         date: '27-03-2021'
     },
 ]
+
+function showReviewTotal (value: number, reviewer: string) {
+    reviewTotalDisplay.innerHTML = 'review total' + value.toString() + '| last review by' + reviewer
+}
+
+showReviewTotal(reviews.length, reviews[0].name)
